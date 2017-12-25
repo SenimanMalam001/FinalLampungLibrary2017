cdd2b7351e2ad5504905498cefb6562e06c95a70
SenimanMalam001 <elfinsanjaya12@gmail.com>
2017-12-25 09:58:54 +0700

    25-12-2017 10.00

--- app/src/main/java/senimanmalam/com/finallampunglibrary2017/HomeActivity.java
+++ app/src/main/java/senimanmalam/com/finallampunglibrary2017/HomeActivity.java
@@ -1,7 +1,12 @@
 package senimanmalam.com.finallampunglibrary2017;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +14,49 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tittle_bar);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        HomeFragment home = new HomeFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fram, home);
+        fragmentTransaction.commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+
+                case R.id.navigation_home:
+                    HomeFragment home = new HomeFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.fram, home);
+                    fragmentTransaction.commit();
+                    return true;
+
+                case R.id.navigation_profile:
+                    Profilefragment profile= new Profilefragment();
+                    FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction2.replace(R.id.fram, profile);
+                    fragmentTransaction2.commit();
+                    return true;
+                case R.id.navigation_category:
+                    CategoryFragment category = new CategoryFragment();
+                    FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction3.replace(R.id.fram, category);
+                    fragmentTransaction3.commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
+
 }
---
+++ app/src/main/java/senimanmalam/com/finallampunglibrary2017/Profilefragment.java
@@ -0,0 +1,27 @@
+package senimanmalam.com.finallampunglibrary2017;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by asus on 25/12/2017.
+ */
+
+public class Profilefragment extends Fragment {
+
+
+    public Profilefragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
